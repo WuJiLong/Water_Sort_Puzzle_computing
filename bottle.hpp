72a1357ca16642d94b4a144d2587a3488864f1c0
WuJiLong <1105105330@nkust.edu.tw>
2021-12-13 17:25:21 +0800

    Update bottle.hpp

--- bottle.hpp
+++ bottle.hpp
@@ -12,7 +12,7 @@ class bottle{
         ~bottle();
         char get_top_color();//取得瓶子最頂部的顏色
         bool is_finish();//瓶子是否裝滿且只有一種顏色
-		bool is_onecolor();//瓶子是否只有一種顏色
+	bool is_onecolor();//瓶子是否只有一種顏色
         bool is_full();//瓶子是否為滿的
         bool is_empty();//瓶子是否為空的
         int operator<<(bottle&);
